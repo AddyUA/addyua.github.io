309e1465bde1d7a16ec3b56b6f973bd925e9de34
alexandr.podygrushniy <renzoha@gmail.com>
2018-12-13 18:30:13 +0200

    adding bootstrap and reworked title

--- js/js-home2/js/scrypt.js
+++ js/js-home2/js/scrypt.js
@@ -1,7 +1,9 @@
-
 let login = prompt('Введите логин');
+
 	if (login === 'Админ') {
+	
 		let password = prompt('Введите пароль');
+
 		if (password === 'Чёрный Властелин') {
 			alert('Добро пожаловать!');
 		} else if (password === null) {
@@ -9,8 +11,10 @@ let login = prompt('Введите логин');
 		} else {
 			alert ('Пароль невереный');
 		}
+
 	} else if (login === null) {
 		alert('Вход отменен');
+
 	} else {
 		alert('Я вас не знаю');
 	}
